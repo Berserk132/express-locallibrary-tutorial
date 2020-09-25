bc205193a134eaaf6b78d7426259ac3f2121cc3f
Ahmed Sayed <ahmedsayed22988@yahoo.com>
2020-09-25 22:25:11 +0200

    First version of application moved into github

---
+++ models/bookinstance.js
@@ -0,0 +1,28 @@
+var mongoose = require("mongoose");
+var moment = require('moment');
+var Schema = mongoose.Schema;
+
+var BookInstanceSchema = new Schema({
+  book: { type: Schema.Types.ObjectId, ref: "Book" }, //reference to the associated book
+  imprint: { type: String },
+  status: {
+    type: String,
+    required: true,
+    enum: ["Available", "Maintenance", "Loaned", "Reserved"],
+    default: "Maintenance",
+  },
+  due_back: { type: Date, default: Date.now },
+});
+
+// Virtual for bookinstance's URL
+BookInstanceSchema.virtual("url").get(function () {
+  return "/catalog/bookinstance/" + this._id;
+});
+
+// Virtual for formatting the date
+BookInstanceSchema.virtual('due_back_formatted').get(() => {
+  return moment(this.due_back).format('MMMM Do, YYYY')
+})
+
+//Export model
+module.exports = mongoose.model("BookInstance", BookInstanceSchema);
